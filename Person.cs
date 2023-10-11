af85d3a7ff6b4db194c68b379956e405f9e321bf
Liam Townsley <liamtownsley43@gmail.com>
2023-10-11 11:42:50 +0100

    init: Initial Commit

---
+++ Person.cs
@@ -0,0 +1,7 @@
+namespace Contacts.person {
+    public class Person {
+        public string Name { get; set; }
+        public double Age { get; set; }
+        public double SSN { get; set; }
+    }
+}
